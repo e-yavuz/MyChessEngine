36cbe198b341fd2e576ee62d697cd1dfe8ce61bc
EmreYavuz45661 <eyavuz45661@gmail.com>
2024-06-05 13:26:17 +0000

    refactored search

--- src/search.go
+++ src/search.go
@@ -10,79 +10,98 @@ const (
 	MAX_VALUE  = 2147483648
 )
 
-func (board *Board) Search(depth, plyFromRoot byte, alpha, beta int, bestMove Move, cancelChannel chan int) (Move, int) {
-	hashF := hashfALPHA
+var bestMoveThisIteration, bestMove Move
+var bestEvalThisIteration, bestEval int
 
+func (board *Board) StartSearch(cancelChannel chan int) (Move, int, byte) {
+	var depth byte = 1
+	bestMove = NULL_MOVE
+
+	for {
+		bestMoveThisIteration = NULL_MOVE
+		bestEvalThisIteration = MIN_VALUE
+		board.Search(depth, 0, MIN_VALUE, MAX_VALUE, cancelChannel)
+
+		if bestMoveThisIteration != NULL_MOVE {
+			depth++
+			bestMove = bestMoveThisIteration
+			bestEval = bestEvalThisIteration
+		}
+
+		select {
+		case <-cancelChannel:
+			return bestMove, bestEval, depth
+		default:
+		}
+	}
+}
+
+func (board *Board) Search(depth, plyFromRoot byte, alpha, beta int, cancelChannel chan int) int {
 	// Check if the search has been cancelled
 	select {
 	case <-cancelChannel:
-		return NULL_MOVE, 0
+		return 0
 	default:
 	}
 
-	if ttMove, ttScore := ProbeHash(depth, alpha, beta, board.GetTopState().ZobristKey); ttScore != MIN_VALUE {
-		return ttMove, ttScore
+	if ttScore := ProbeHash(depth, alpha, beta, board.GetTopState().ZobristKey); ttScore != MIN_VALUE {
+		if plyFromRoot == 0 {
+			bestMoveThisIteration = GetEntry(board.GetTopState().ZobristKey).best
+			bestEvalThisIteration = ttScore
+		}
+		return ttScore
 	}
 
-	// Check if the search reached the maximum depth
 	if depth == 0 {
 		eval := board.QuiescenceSearch(alpha, beta, cancelChannel)
-		RecordHash(0, eval, hashfEXACT, NULL_MOVE, board.GetTopState().ZobristKey)
-		return NULL_MOVE, eval
+		// RecordHash(0, eval, hashfEXACT, NULL_MOVE, board.GetTopState().ZobristKey)
+		return eval
 	}
 
-	// Generate the total move list, don't sort Quiet moves because a lot of them are just positioning?
-	totalMoveList := append(*board.generateMoves(CAPTURE), *board.generateMoves(QUIET)...)
-
-	sort.Slice(totalMoveList, func(i, j int) bool {
-		return totalMoveList[i] > totalMoveList[j]
-	})
-
-	// if best_move != NULL_MOVE, add it to the front of the list
-	if bestMove != NULL_MOVE {
-		totalMoveList = append([]Move{bestMove}, totalMoveList...)
-	}
+	totalMoveList := *getAllMoves(board, plyFromRoot)
 
 	if len(totalMoveList) == 0 {
 		if board.inCheck() {
-			return NULL_MOVE, MATE_SCORE + int(plyFromRoot)
+			return MATE_SCORE + int(plyFromRoot) // Checkmate
 		} else {
-			return NULL_MOVE, 0
+			return 0 // Stalemate
 		}
 	}
+
+	hashF := hashfALPHA
+	bestMoveThisPath := NULL_MOVE
 
 	for _, move := range totalMoveList {
 		board.MakeMove(move)
 
-		_, score := board.Search(depth-1, plyFromRoot+1, -beta, -alpha, NULL_MOVE, cancelChannel)
-		score = -score
+		score := -board.Search(depth-1, plyFromRoot+1, -beta, -alpha, cancelChannel)
 
 		board.UnMakeMove()
 
 		// Check if the search has been cancelled
 		select {
 		case <-cancelChannel:
-			if alpha == MIN_VALUE {
-				return NULL_MOVE, 0
-			} else {
-				return bestMove, alpha
-			}
+			return 0
 		default:
 		}
 
 		if score >= beta {
 			RecordHash(depth, beta, hashfBETA, move, board.GetTopState().ZobristKey)
-			return NULL_MOVE, beta
+			return beta
 		}
 		if score > alpha {
-			bestMove = move
+			bestMoveThisPath = move
 			hashF = hashfEXACT
 			alpha = score
+			if plyFromRoot == 0 {
+				bestMoveThisIteration = move
+				bestEvalThisIteration = score
+			}
 		}
 	}
 
-	RecordHash(depth, alpha, hashF, bestMove, board.GetTopState().ZobristKey)
-	return bestMove, alpha
+	RecordHash(depth, alpha, hashF, bestMoveThisPath, board.GetTopState().ZobristKey)
+	return alpha
 }
 
 func (board *Board) QuiescenceSearch(alpha, beta int, cancelChannel chan int) int {
@@ -107,22 +126,41 @@ func (board *Board) QuiescenceSearch(alpha, beta int, cancelChannel chan int) in
 
 	for _, move := range captureMoveList {
 		board.MakeMove(move)
-		score := -board.QuiescenceSearch(-beta, -alpha, cancelChannel)
+		eval := -board.QuiescenceSearch(-beta, -alpha, cancelChannel)
 		board.UnMakeMove()
 
-		select { // Check if the search has been cancelled
-		case <-cancelChannel:
-			return 0
-		default:
-		}
-
-		if score >= beta {
+		if eval >= beta {
 			return beta
 		}
-		if score > alpha {
-			alpha = score
+		if eval > alpha {
+			alpha = eval
 		}
 	}
 
 	return alpha
+}
+
+func getAllMoves(board *Board, plyFromRoot byte) *[]Move {
+	var totalMoveList []Move
+	captureList := *board.generateMoves(CAPTURE)
+	quietList := *board.generateMoves(QUIET)
+
+	if bestMove != NULL_MOVE && plyFromRoot == 0 {
+		totalMoveList = make([]Move, 0, len(captureList)+len(quietList)+1)
+		totalMoveList = append(totalMoveList, bestMove)
+		totalMoveList = append(totalMoveList, captureList...)
+		totalMoveList = append(totalMoveList, quietList...)
+		sort.Slice(totalMoveList[1:], func(i, j int) bool {
+			return totalMoveList[i] > totalMoveList[j]
+		})
+	} else {
+		totalMoveList = make([]Move, 0, len(captureList)+len(quietList))
+		totalMoveList = append(totalMoveList, captureList...)
+		totalMoveList = append(totalMoveList, quietList...)
+		sort.Slice(totalMoveList, func(i, j int) bool {
+			return totalMoveList[i] > totalMoveList[j]
+		})
+	}
+
+	return &totalMoveList
 }
