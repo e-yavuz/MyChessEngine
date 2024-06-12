e1cd5bd23ff0347e9ac3247d2bb0f80b1070600b
EmreYavuz45661 <eyavuz45661@gmail.com>
2024-06-12 11:58:27 +0200

    added mgphase and egphase to pesto eval return

--- src/engine/evaluate.go
+++ src/engine/evaluate.go
@@ -150,7 +150,7 @@ func (board *Board) Evaluate() (retval int) {
 	/******************
 	1. Piece evaluation
 	*******************/
-	pieceVal, mgPhase, _ := PeSTOTableEval(board)
+	pieceVal, _, _ := PeSTOTableEval(board)
 
 	retval += pieceVal
 
@@ -158,7 +158,7 @@ func (board *Board) Evaluate() (retval int) {
 }
 
 // Inspired heavily by https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function, calculate with White being positive
-func PeSTOTableEval(board *Board) (retval int) {
+func PeSTOTableEval(board *Board) (retval, mgPhase, egPhase int) {
 	var mg, eg [2]int
 	var gamePhase int
 
@@ -183,7 +183,7 @@ func PeSTOTableEval(board *Board) (retval int) {
 		mgScore *= -1
 		egScore *= -1
 	}
-	mgPhase := min(24, gamePhase) /* in case of early promotion */
-	egPhase := 24 - mgPhase
-	return (mgScore*mgPhase + egScore*egPhase) / 24
+	mgPhase = min(24, gamePhase) /* in case of early promotion */
+	egPhase = 24 - mgPhase
+	return (mgScore*mgPhase + egScore*egPhase) / 24, mgPhase, egPhase
 }
