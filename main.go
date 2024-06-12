caf59c4357457cf95af590db3156c2a801e28c02
EmreYavuz45661 <eyavuz45661@gmail.com>
2024-06-12 11:55:44 +0200

    misc

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	runTest("./bots/v1", "./bots/v5", "src/testgames/testgames_highquality.txt", 0, 20)
-	// makeUCI()
+	// runTest("./bots/v5", "./bots/v6", "src/testgames/testgames_highquality.txt", 150, 200)
+	makeUCI()
 }
 
 func runTest(engine1, engine2, testgamesPath string, startindex, numgames int) {
