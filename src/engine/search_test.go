e39c8645bafa32461b639b47256278ec69949936
EmreYavuz45661 <eyavuz45661@gmail.com>
2024-07-12 15:33:39 +0200

    more search tests

--- src/engine/search_test.go
+++ src/engine/search_test.go
@@ -5,18 +5,40 @@ import (
 	"time"
 )
 
-func Test_SearchStartPosition_3s(t *testing.T) {
+func Test_SearchStartPosition_20s(t *testing.T) {
 	InitMagicBitBoardTable("../../magic_rook", "../../magic_bishop")
 	InitZobristTable()
 	InitPeSTO()
 
 	test := InitStartBoard()
-	cancelChannel := make(chan int)
+	TTReset(test, DefaultTTMBSize)
+	DebugMode = true
+	cancelChannel := make(chan struct{})
 
 	startTime := time.Now()
 	go func() {
-		time.Sleep(time.Duration(3000) * time.Millisecond)
+		time.Sleep(time.Duration(20000) * time.Millisecond)
 		close(cancelChannel)
 	}()
 	test.StartSearch(startTime, cancelChannel)
+	DebugMode = false
+}
+
+func Test_SearchPosition1_20s(t *testing.T) {
+	InitMagicBitBoardTable("../../magic_rook", "../../magic_bishop")
+	InitZobristTable()
+	InitPeSTO()
+
+	test := InitFENBoard("qrb5/rk1p1K2/p2P4/Pp6/1N2n3/6p1/5nB1/6b1 w - - 0 1")
+	TTReset(test, DefaultTTMBSize)
+	DebugMode = true
+	cancelChannel := make(chan struct{})
+
+	startTime := time.Now()
+	go func() {
+		time.Sleep(time.Duration(20000) * time.Millisecond)
+		close(cancelChannel)
+	}()
+	test.StartSearch(startTime, cancelChannel)
+	DebugMode = false
 }
